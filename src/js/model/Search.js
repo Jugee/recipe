dfb18906304eae0a3d3ae0b04c6a996fbddc36c1
Osor Chimeddorj <jtosor97@gmail.com>
2020-08-07 19:43:52 +0800

    Huudaslalt bolon search model implement hiine

--- src/js/model/Search.js
+++ src/js/model/Search.js
@@ -2,23 +2,21 @@ require("@babel/polyfill");
 import axios from "axios";
 
 export default class Search {
-    constructor(query) {
-        this.query = query;
+  constructor(query) {
+    this.query = query;
+  }
 
+  async doSearch() {
+    try {
+      let result = await axios(
+        "https://forkify-api.herokuapp.com/api/search?q=" + this.query
+      );
+
+      this.result = result.data.recipes;
+
+      return this.result;
+    } catch (error) {
+      console.log("Асуудал гарлаа : " + error);
     }
-
-    async doSearch(search) {
-        try {
-            let result = await axios(
-                "https://forkify-api.herokuapp.com/api/search?q=" + this.query
-            );
-            this.result = result.data.recipes;
-
-            return this.result
-
-
-        } catch (error) {
-            console.log("Алдаа гарлаа=>>")
-        }
-    }
+  }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,35 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const path = require('path');
-
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry: './src/js/index.js',
-    output: {
-        filename: 'js/main.js',
-        path: path.resolve(__dirname, 'docs'),
-    },
-    devServer: {
-        contentBase: './docs',
-    },
-    plugins: [
-        new HtmlWebpackPlugin({  // Also generate a test.html
-            filename: 'index.html',
-            template: 'src/index.html'
-        }),
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.m?js$/,
-                exclude: /(node_modules|bower_components)/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env']
-                    }
-                }
-            }
-        ]
-    }
+	mode: "development",
+	entry: "./src/js/index.js",
+	output: {
+		filename: "js/main.js",
+		path: path.resolve(__dirname, "docs"),
+	},
+	devtool: "inline-source-map",
+	devServer: {
+		contentBase: "./docs",
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			filename: "index.html",
+			template: "src/index.html",
+		}),
+	],
+	module: {
+		rules: [
+			{
+				test: /\.m?js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: "babel-loader",
+					options: {
+						presets: ["@babel/preset-env"],
+					},
+				},
+			},
+		],
+	},
 };
